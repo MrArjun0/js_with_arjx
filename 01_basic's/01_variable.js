45a8ee406fb88296a3fa419b78a33563457443f4
Mr. Arjun P <119003081+MrArjun0@users.noreply.github.com>
2024-11-18 19:21:18 +0000

    next commit

---
+++ 01_basic's/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "arjun023@gmail.com"
+var accountPassword = "arjun4523"
+ accountCity = "Azamgarh"
+
+ accountEmail = "adfja@gmail.com"
+ accountPassword = "238sf"
+ accountCity = "varanasi"
+ let accoutState = "Uttar Pradesh "
+// let have limited scope 
+// var have wide sope to change the value so prefer to not to use var
+// accountId = 2  const cant be change 
+console.log(accountId)
+console.table([accountEmail,accountId,accountPassword,accountCity , accoutState]);
---
+++ 01_basic's/02_dataType.js
@@ -0,0 +1,13 @@
+"use strict"
+//   alert(23-2)
+
+let age = 23
+let name = "arjun"
+let isLogedIn = true
+
+let number = 234824
+let bigint = 32139237948924924n
+
+let state = null
+// console.table(typeof([state,age,name,isLogedIn]))
+console.table([age , name , isLogedIn, number,bigint ,state])
