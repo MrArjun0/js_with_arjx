a71ac099982a86281ea0a365ebd13962c6b39a43
Mr. Arjun P <119003081+MrArjun0@users.noreply.github.com>
2024-11-19 02:06:44 +0000

    new

--- 01_basic's/03_conversionOperation.js
+++ 01_basic's/03_conversionOperation.js
@@ -17,3 +17,19 @@ let aNumber = undefined
 let stringNumber = String(aNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+ let a = 3 
+ let b = ++a  // prefix operator
+//  console.log(`a:${a}, b:${b}`)
+
+ let c = 6 
+ let d = c++
+//  console.log(`c:${c} , d:${d}`)
+
+console.log(1 != 1 )
+
+console.log( 'hello' != 'hello')
+
+console.log ( 0 != false)
+
+console.log('1' != 1)
