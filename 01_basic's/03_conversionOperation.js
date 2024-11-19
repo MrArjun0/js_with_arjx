e6d8d43ad515688d5f5d2a4435fd63f92fbd5231
Mr. Arjun P <119003081+MrArjun0@users.noreply.github.com>
2024-11-19 01:00:08 +0000

    conversion of datatypes

---
+++ 01_basic's/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = '33adhf'
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isloggedIn = 0// it may be any dataType 
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let aNumber = undefined
+
+let stringNumber = String(aNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
